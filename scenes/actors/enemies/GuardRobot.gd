917120ff9173868d22d82fb5e4f67bf1ba865d31
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-23 21:03:24 -0600

    Finished basic robot ai
    Needs polish, but the skeleton is finished

--- scenes/actors/enemies/GuardRobot.gd
+++ scenes/actors/enemies/GuardRobot.gd
@@ -3,8 +3,9 @@ extends Enemy
 
 export(float, 0.1, 1000, 0.1) var detection_radius : float setget set_detection_radius
 
+enum {STATE_INVALID = -1, STATE_IDLE, STATE_CHARGE, STATE_MOVEBACK}
+
 onready var timer : Timer = $Timer
-var blackboard
 
 var _distance_met := 0.0
 var _state := 0
@@ -16,6 +17,8 @@ func _ready() -> void:
 		return
 	
 	set_meta('initial_position', global_position)
+	
+	$Frames.play('Idle')
 
 func _draw() -> void:
 	if Engine.editor_hint:
@@ -28,13 +31,13 @@ func _physics_process(delta: float) -> void:
 
 func _exec_state(delta: float) -> void:
 	match _state:
-		0:
+		STATE_IDLE:
 			# Triggered when the player enters its range
 			var player := Game.get_player()
 			if Geometry.is_point_in_circle(player.global_position, global_position, detection_radius):
-				_state += 1
+				_state = STATE_CHARGE
 				direction = global_position.direction_to(player.global_position)
-		1:
+		STATE_CHARGE:
 			# Charge at the player
 			
 			if _distance_met >= detection_radius:
@@ -42,28 +45,29 @@ func _exec_state(delta: float) -> void:
 				velocity.x = lerp(velocity.x, 0, delta * 10)
 				if is_zero_approx(velocity.x):
 					direction.x = -direction.x
-					_state += 1
+					_state = STATE_MOVEBACK
 					_distance_met = 0
+					$Frames.play('Move', true)
 			else:
 				velocity.x = lerp(velocity.x, speed_cap.x * direction.x, delta * 5)
-		2:
+		STATE_MOVEBACK:
 			# Move back into position
 			
 			var dest : Vector2 = get_meta('initial_position')
 			if Math.is_almost_equal(global_position.x, dest.x, 0.003):
 				if Math.is_almost_zero(velocity.x):
+					# Go into idle state
 					global_position = dest
 					velocity.x = 0
 					timer.start()
-					_state += 1
+					_state = STATE_INVALID
+					direction.x = 0
+					$Frames.play('Idle')
+					_distance_met = 0
 				else:
 					velocity.x = lerp(velocity.x, 0, delta * 30)
 			else:
 				velocity.x = lerp(velocity.x, 50.0 * direction.x, delta * 10)
-		3:
-			# Wait a bit
-			if is_zero_approx(timer.time_left):
-				_state = 0
 
 func _on_damaged(_stats: Stats) -> void:
 	queue_free()
@@ -78,3 +82,6 @@ func move_actor():
 func set_detection_radius(r: float):
 	detection_radius = r
 	update()
+
+func _on_Timer_timeout() -> void:
+	_state = STATE_IDLE
