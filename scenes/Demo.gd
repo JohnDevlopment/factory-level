e9ccfd1802187902818a7bef17a4d2eebe3f8bae
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-12-06 13:40:25 -0600

    Demo scene refers to the player as "Bob" now

--- scenes/Demo.gd
+++ scenes/Demo.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
-var debug_overlay
-var explorer
+var Bob : Actor
+var DebugOverlay
+var EnergyBall
 
 func _ready() -> void:
-	explorer = $Actors/Explorer
-	debug_overlay = $DebugOverlay
-	debug_overlay.add_stat("Velocity", $Actors/EnergyBall, "velocity", false)
+	NodeMapper.map_nodes(self)
+	
+	#DebugOverlay.add_stat("Ball Velocity", EnergyBall, "velocity", false)
+	DebugOverlay.add_stat("Bob Velocity", Bob, "velocity", false)
+	DebugOverlay.add_stat("Bob Direction", Bob, "direction", false)
+	DebugOverlay.add_stat("Bob Input Vector", Bob, "input_vector", false)
+	#DebugOverlay.add_stat("Current State", Bob, "current_state", true)
 
 func _unhandled_key_input(event: InputEventKey) -> void:
 	if event.is_action_pressed('reset'):
