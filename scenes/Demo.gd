b6433afa6f35134f42a11ae7d15fc9a6960d3339
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-12-07 13:20:33 -0600

    Tilemap changed

--- scenes/Demo.gd
+++ scenes/Demo.gd
@@ -7,11 +7,9 @@ var EnergyBall
 func _ready() -> void:
 	NodeMapper.map_nodes(self)
 	
-	#DebugOverlay.add_stat("Ball Velocity", EnergyBall, "velocity", false)
 	DebugOverlay.add_stat("Bob Velocity", Bob, "velocity", false)
 	DebugOverlay.add_stat("Bob Direction", Bob, "direction", false)
 	DebugOverlay.add_stat("Bob Input Vector", Bob, "input_vector", false)
-	#DebugOverlay.add_stat("Current State", Bob, "current_state", true)
 
 func _unhandled_key_input(event: InputEventKey) -> void:
 	if event.is_action_pressed('reset'):
