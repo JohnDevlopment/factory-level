717598566b3662e6e3fbb646bbb16a96162ca922
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-12-28 12:20:46 -0600

    Debug actions only done on debug builds

--- scenes/Demo.gd
+++ scenes/Demo.gd
@@ -4,13 +4,24 @@ var Bob : Actor
 var DebugOverlay
 var EnergyBall
 
+onready var debug_console = $DebugOverlay/DebugConsole
+
 func _ready() -> void:
 	NodeMapper.map_nodes(self)
 	
-	DebugOverlay.add_stat("Bob Velocity", Bob, "velocity", false)
-	DebugOverlay.add_stat("Bob Direction", Bob, "direction", false)
-	DebugOverlay.add_stat("Bob Input Vector", Bob, "input_vector", false)
+	if OS.has_feature('debug'):
+		DebugOverlay.add_stat("Bob Velocity", Bob, "velocity", false)
+		DebugOverlay.add_stat("Bob Direction", Bob, "direction", false)
+		DebugOverlay.add_stat("Bob Input Vector", Bob, "input_vector", false)
+	else:
+		(DebugOverlay as Object).queue_free()
+		DebugOverlay = null
+		
+	(debug_console as Object).queue_free()
+	debug_console = null
 
 func _unhandled_key_input(event: InputEventKey) -> void:
-	if event.is_action_pressed('reset'):
-		$EnergyBall.set_rigid_position(Vector2(248, 350))
+	if OS.has_feature('debug'):
+		# debug commands
+		if event.is_action_pressed('console'):
+			debug_console.activate()
