9c7d25357efba9d5f0ab4fd525c835e66febd0ad
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-01-25 10:30:58 -0600

    Snap BaseButton to a vector when positioning it
    * Add 'position_snap' property
    
    * Moved _process, _physics_process, and _ready to _notification
    
    * Property is documented

--- scenes/buttons/BaseButton.gd
+++ scenes/buttons/BaseButton.gd
@@ -26,6 +26,11 @@ export var toggled := false setget set_toggled
 # @type bool
 export var sticky := true
 
+## Vector to snap the button to
+# @type Vector2
+# @desc When moving the button within the editor, its position is snapped to this.
+export var position_snap := Vector2(7, 7)
+
 onready var sprite = $Frames
 onready var animation_player = $AnimationPlayer
 
@@ -40,27 +45,39 @@ func set_toggled(flag: bool) -> void:
 	set_process(true)
 	emit_signal('toggle_status_changed', flag)
 
-func _process(_delta: float) -> void:
-	if Engine.editor_hint or not _init:
-		if toggled:
-			$Frames.play('Glow')
-		else:
-			$Frames.animation = SPRITE_ANIMATION
-	else:
-		# Play the sprite animation either forwards or backwards, then animate the hitbox
-		sprite.play(SPRITE_ANIMATION, !toggled)
-		var anim_speed : float = 1 if toggled else -1
-		animation_player.play(HITBOX_ANIMATION, -1, anim_speed, !toggled)
-	set_process(false)
-
-func _ready() -> void:
-	set_physics_process(false)
-	if Engine.editor_hint: return
-	for node in get_children():
-		if node.has_method('do_command'):
-			_commands.append(funcref(node, 'do_command'))
-	$Frames.connect('animation_finished', self, '_on_Frames_animation_finished')
-	$AnimationPlayer.connect('animation_finished', self, '_on_AnimationPlayer_animation_finished')
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_READY:
+			set_physics_process(false)
+			if Engine.editor_hint:
+				set_notify_transform(true)
+				return
+			for node in get_children():
+				if node.has_method('do_command'):
+					_commands.append(funcref(node, 'do_command'))
+			$Frames.connect('animation_finished', self, '_on_Frames_animation_finished')
+			$AnimationPlayer.connect('animation_finished', self, '_on_AnimationPlayer_animation_finished')
+		NOTIFICATION_PHYSICS_PROCESS:
+			if not toggled and _player.is_on_floor():
+				var dir := global_position.direction_to(_player.call('get_bottom_edge'))
+				_dot = dir.dot(Vector2.UP)
+				if _dot >= 0.5:
+					set_toggled(true)
+		NOTIFICATION_PROCESS:
+			if Engine.editor_hint or not _init:
+				if toggled:
+					$Frames.animation = 'Glow'
+				else:
+					$Frames.animation = SPRITE_ANIMATION
+			else:
+				# Play the sprite animation either forwards or backwards, then animate the hitbox
+				sprite.play(SPRITE_ANIMATION, !toggled)
+				var anim_speed : float = 1 if toggled else -1
+				animation_player.play(HITBOX_ANIMATION, -1, anim_speed, !toggled)
+			set_process(false)
+		NOTIFICATION_TRANSFORM_CHANGED:
+			var s := global_position.snapped(position_snap)
+			global_position = s
 
 func _do_commands() -> void:
 	for fr in _commands:
@@ -70,13 +87,6 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_action_pressed('debug'):
 			set_toggled(!toggled)
-
-func _physics_process(_delta: float) -> void:
-	if not toggled and _player.is_on_floor():
-		var dir := global_position.direction_to(_player.call('get_bottom_edge'))
-		_dot = dir.dot(Vector2.UP)
-		if _dot >= 0.5:
-			set_toggled(true)
 
 func _on_DetectPlayer_body_entered(body: Node) -> void:
 	set_physics_process(true)
