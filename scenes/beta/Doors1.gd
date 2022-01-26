6fd6addf5708dc536b22c3b2b8df3b532479b6f3
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-01-26 14:30:19 -0600

    Update Doors1 beta level

--- scenes/beta/Doors1.gd
+++ scenes/beta/Doors1.gd
@@ -1,8 +1,4 @@
 extends "res://scenes/Room.gd"
 
 func _ready() -> void:
-	if OS.has_feature('debug'):
-		var overlay = $DebugOverlay
-		overlay.add_stat('Bob Velocity', $Actors/Explorer, 'velocity', false)
-		overlay.add_stat('Button Dot Product', $BaseButton, '_dot', false)
-		overlay.add_stat('Button Toggled', $BaseButton, 'toggled', false)
+	NodeMapper.map_nodes(self)
