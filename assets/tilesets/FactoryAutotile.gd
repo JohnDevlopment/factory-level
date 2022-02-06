49e82e3161c409a23dc596f0866cf9824aa67ca9
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-02-06 09:56:03 -0600

    Add factory slopes texture to the tileset
    Tileset: Moved slope tiles into separate texture

--- assets/tilesets/FactoryAutotile.gd
+++ assets/tilesets/FactoryAutotile.gd
@@ -4,18 +4,24 @@ extends TileSet
 #const BIND_ALL := 511
 
 enum {
-	FOREGROUND = 0,
-	LEFT_FACING_LONG_SLOPE = 5,
-	RIGHT_FACING_LONG_SLOPE = 6
+	FOREGROUND              = 0,
+	LEFT_FACING_SLOPE       = 5,
+	LEFT_FACING_LONG_SLOPE  = 6,
+	RIGHT_FACING_LONG_SLOPE = 7,
+	RIGHT_FACING_SLOPE      = 8
 }
 
 const FILL_TILE := Vector2(9, 2)
 
 const BINDS := {
+	LEFT_FACING_SLOPE: [FOREGROUND],
 	LEFT_FACING_LONG_SLOPE: [FOREGROUND],
+	RIGHT_FACING_SLOPE: [FOREGROUND],
 	RIGHT_FACING_LONG_SLOPE: [FOREGROUND],
-	FOREGROUND: [LEFT_FACING_LONG_SLOPE, RIGHT_FACING_LONG_SLOPE]
+	FOREGROUND: [LEFT_FACING_LONG_SLOPE, RIGHT_FACING_LONG_SLOPE, LEFT_FACING_SLOPE, RIGHT_FACING_SLOPE]
 }
+
+enum TileDirection {LEFT, UP, RIGHT, DOWN}
 
 func _is_tile_bound(drawn_id: int, neighbor_id: int) -> bool:
 	if drawn_id in BINDS:
@@ -23,7 +29,7 @@ func _is_tile_bound(drawn_id: int, neighbor_id: int) -> bool:
 	return false
 
 func _forward_subtile_selection(autotile_id: int, bitmask: int, tilemap: Object, tile_location: Vector2):
-	var subtiles := get_subtiles(autotile_id)
+	#var subtiles := get_subtiles(autotile_id)
 	var tm := tilemap as TileMap
 	match autotile_id:
 		FOREGROUND:
@@ -31,70 +37,82 @@ func _forward_subtile_selection(autotile_id: int, bitmask: int, tilemap: Object,
 				if tm.get_cellv(tile_location + Vector2.UP) in [LEFT_FACING_LONG_SLOPE, RIGHT_FACING_LONG_SLOPE]:
 					# tile above is a slope
 					tm.set_cellv(tile_location, autotile_id)
-					return Vector2(9, 2)
+					return FILL_TILE
 		LEFT_FACING_LONG_SLOPE:
-			if bitmask == BIND_CENTER:
-				# icon tile when there are no adjacent tiles
-				tm.set_cellv(tile_location, autotile_id)
-				return autotile_get_icon_coordinate(autotile_id)
-			
-			var neighbor_tiles := get_adjacent_tiles(tile_location, tilemap)
-			if bitmask & BIND_LEFT:
-				# check that the tile to the left has the same ID
-				if (neighbor_tiles[0].id as int) == autotile_id:
-					#var coord := get_subtile_left(tm, autotile_id, tile_location)
-					var coord : Vector2 = neighbor_tiles[0].autotile_coord
-					var new_tile := autotile_id
-					if coord.x <= 3.0:
-						coord.x += 1.0
-					else:
-						coord = FILL_TILE
-						new_tile = FOREGROUND
-					
-					tm.set_cellv(tile_location, new_tile)
-					#coord.x = (coord.x + 1.0) if coord.x < 3.0 else 4.0
-					return coord
-			if bitmask & BIND_TOP:
-				# check that the tile above has the same ID
-				if (neighbor_tiles[1].id as int) == autotile_id:
-					var new_tile_id := autotile_id
-					var coord : Vector2 = neighbor_tiles[1].autotile_coord
-					print("tile above autotile coordinate: ", coord)
-					print("current tile location: ", tile_location)
-					if coord.x == 0.0:
-						coord.x = 3.0
-					elif coord.x >= 2.0:
-						new_tile_id = FOREGROUND
-						coord = Vector2(9, 2)
-					else:
-						coord.x += 1
-					#print("new coord: ", coord)
-					tm.set_cellv(tile_location, new_tile_id)
-					return coord
-			# Fallback when all other conditions fail; use the icon texture
-			tm.set_cellv(tile_location, autotile_id)
-			return autotile_get_icon_coordinate(autotile_id)
+#			var coord = slope_tile(autotile_id, tile_location, bitmask, {
+#				tilemap = tilemap,
+#				dir_horz = TileDirection.LEFT,
+#				bind_horz = BIND_LEFT,
+#				dir_vert = TileDirection.UP,
+#				bind_vert = BIND_TOP,
+#				test = "sid <= 3.0"
+#			})
+			#if coord is Vector2: return coord
+			pass
+#			if bitmask == BIND_CENTER:
+#				# icon tile when there are no adjacent tiles
+#				tm.set_cellv(tile_location, autotile_id)
+#				return autotile_get_icon_coordinate(autotile_id)
+#
+#			var neighbor_tiles := get_adjacent_tiles(tile_location, tilemap)
+#			if bitmask & BIND_LEFT:
+#				# check that the tile to the left has the same ID
+#				if (neighbor_tiles[TileDirection.LEFT].id as int) == autotile_id:
+#					var coord : Vector2 = neighbor_tiles[TileDirection.LEFT].autotile_coord
+#					var new_tile := autotile_id
+#					if coord.x <= 3.0:
+#						coord.x += 1.0
+#					else:
+#						coord = FILL_TILE
+#						new_tile = FOREGROUND
+#
+#					tm.set_cellv(tile_location, new_tile)
+#					return coord
+#			if bitmask & BIND_TOP:
+#				# check that the tile above has the same ID
+#				if (neighbor_tiles[TileDirection.UP].id as int) == autotile_id:
+#					var new_tile_id := autotile_id
+#					var coord : Vector2 = neighbor_tiles[TileDirection.UP].autotile_coord
+#					if coord.x == 0.0:
+#						coord.x = 3.0
+#					elif coord.x >= 2.0:
+#						new_tile_id = FOREGROUND
+#						coord = Vector2(9, 2)
+#					else:
+#						coord.x += 1
+#					tm.set_cellv(tile_location, new_tile_id)
+#					return coord
+#			# Fallback when all other conditions fail; use the icon texture
+#			tm.set_cellv(tile_location, autotile_id)
+#			return autotile_get_icon_coordinate(autotile_id)
+
+func create_expression(expression: String, binds: PoolStringArray) -> Expression:
+	var e := Expression.new()
+	var err := e.parse(expression, binds)
+	if err:
+		printerr(e.get_error_text())
+	return e
 
 # Returns the coordinate of the autotile subtile left of v.
 # Returns a negative vector if the two tiles have different IDs.
-func get_subtile_left(tm: TileMap, autotile_id: int, v: Vector2) -> Vector2:
-	return _get_subtile_shifted(tm, autotile_id, v, Vector2.LEFT)
-
-# Returns the coordinate of the autotile subtile right of v.
-# Returns a negative vector if the two tiles have different IDs.
-func get_subtile_right(tm: TileMap, autotile_id: int, v: Vector2) -> Vector2:
-	return _get_subtile_shifted(tm, autotile_id, v, Vector2.RIGHT)
-
-# Returns the coordinate of the autotile subtile left of v.
-# Returns a negative vector if the two tiles have different IDs.
-func get_subtile_up(tm: TileMap, autotile_id: int, v: Vector2) -> Vector2:
-	return _get_subtile_shifted(tm, autotile_id, v, Vector2.UP)
-
-func _get_subtile_shifted(tm: TileMap, autotile_id: int, v: Vector2, o: Vector2) -> Vector2:
-	var tile_location := v + o
-	if tm.get_cellv(tile_location) == autotile_id:
-		return tm.get_cell_autotile_coord(tile_location.x, tile_location.y)
-	return Vector2(-1, -1)
+#func get_subtile_left(tm: TileMap, autotile_id: int, v: Vector2) -> Vector2:
+#	return _get_subtile_shifted(tm, autotile_id, v, Vector2.LEFT)
+#
+## Returns the coordinate of the autotile subtile right of v.
+## Returns a negative vector if the two tiles have different IDs.
+#func get_subtile_right(tm: TileMap, autotile_id: int, v: Vector2) -> Vector2:
+#	return _get_subtile_shifted(tm, autotile_id, v, Vector2.RIGHT)
+#
+## Returns the coordinate of the autotile subtile left of v.
+## Returns a negative vector if the two tiles have different IDs.
+#func get_subtile_up(tm: TileMap, autotile_id: int, v: Vector2) -> Vector2:
+#	return _get_subtile_shifted(tm, autotile_id, v, Vector2.UP)
+#
+#func _get_subtile_shifted(tm: TileMap, autotile_id: int, v: Vector2, o: Vector2) -> Vector2:
+#	var tile_location := v + o
+#	if tm.get_cellv(tile_location) == autotile_id:
+#		return tm.get_cell_autotile_coord(tile_location.x, tile_location.y)
+#	return Vector2(-1, -1)
 
 # Returns an array of tiles that are adjacent to the one at the given cell.
 # Each element is a dictionary:
@@ -123,6 +141,20 @@ func get_adjacent_tiles(tile_location: Vector2, tilemap: TileMap) -> Array:
 		})
 	return result
 
+func get_subtile_coord(sid: int, width: int):
+	return Vector2(sid % width, sid / width)
+
+func get_subtile_coord_from_id(autotile_id: int, sid: int):
+	var width := int(autotile_get_size(autotile_id).x / tile_get_region(autotile_id).size.x)
+	return get_subtile_coord_from_id(sid, width)
+
+func get_subtile_id(autotile_id: int, v: Vector2) -> int:
+	var size := autotile_get_size(autotile_id)
+	var region := tile_get_region(autotile_id)
+	return get_subtile_id_from_vector(v, int(region.size.x / size.x))
+
+func get_subtile_id_from_vector(v: Vector2, width: int) -> int: return int(v.y) * width + int(v.x)
+
 # Helper function for iterating over subtiles
 # Return an array of dictionaries with subtile texture co-ords,
 # and subtile bitmask
@@ -142,7 +174,44 @@ func get_subtiles(autotile_id: int) -> Array:
 			if mask != 0:
 				return_values.push_back({
 					"coord": coord,
-					"mask": mask,
-					"width": end_x
+					"mask": mask
 				})
 	return return_values
+
+func slope_tile(id: int, bitmask: int, loc: Vector2, tilemap: TileMap, reverse: bool = false):
+	if bitmask & BIND_CENTER:
+		tilemap.set_cellv(loc, id)
+		return autotile_get_icon_coordinate(id)
+	
+	
+
+#func _slope_tile(id: int, loc: Vector2, bitmask: int, options: Dictionary):
+#	var tilemap := options.tilemap as TileMap
+#
+#	if bitmask & BIND_CENTER:
+#		tilemap.set_cellv(loc, id)
+#		return autotile_get_icon_coordinate(id)
+#
+#	var neighbor_tiles := get_adjacent_tiles(loc, options.tilemap)
+#	var direction : int = options.dir_horz
+#
+#	if bitmask & (options.bind_horz as int):
+#		if (neighbor_tiles[direction].id as int) == id:
+#			var e := Expression.new()
+#			if e.parse(options.test, ['sid']):
+#				printerr(e.get_error_text())
+#				return
+#			var coord : Vector2 = neighbor_tiles[direction].autotile_coord
+#			var sid := get_subtile_id(id, coord)
+#			var new_tile_id := id
+#			if (e.execute([sid]) as bool):
+#				sid += 1
+#				coord = get_subtile_coord_from_id(id, sid)
+#			else:
+#				coord = FILL_TILE
+#				new_tile_id = FOREGROUND
+#			tilemap.set_cellv(loc, new_tile_id)
+#			return coord
+#
+#	if bitmask & (options.bind_vert as int):
+#		pass
