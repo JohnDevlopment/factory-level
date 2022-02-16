257c8b025b30cb99f7cae7e7f7892143c98fa197
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-02-16 10:03:02 -0600

    Show UI at the start of a level

--- scenes/Room.gd
+++ scenes/Room.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
+var hud : Control
+
 func _ready() -> void:
+	hud = UI.get_hud()
+	if is_instance_valid(hud):
+		hud.show()
+	
 	Game.set_paused(true)
 	TransitionRect.set_alpha(1)
 	TransitionRect.fade_in()
 	yield(TransitionRect, 'fade_finished')
 	Game.set_paused(false)
+
+func _exit_tree() -> void:
+	hud.hide()
