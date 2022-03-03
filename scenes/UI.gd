f7a32f1724e5cd9a93e9dba867e6f52cd419d6a6
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-03-02 22:19:01 -0600

    UI: Add documentation to script

--- scenes/UI.gd
+++ scenes/UI.gd
@@ -1,3 +1,7 @@
+## User interface singleton
+# @name UI
+# @singleton
+# @desc Use this to access anything related to UI.
 extends CanvasLayer
 
 var _hud : Control
@@ -5,4 +9,5 @@ var _hud : Control
 func _ready() -> void:
 	_hud = $HUD
 
+## Returns a reference to the HUD scene.
 func get_hud() -> Control: return _hud
