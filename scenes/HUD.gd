5711cca99a2265053f5072b640d218edb17a2dfb
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-03-02 22:19:01 -0600

    HUD: Add documentation to script

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -1,9 +1,21 @@
+## The HUD (heads-up display) for the player.
+# @name HUD
+# @singleton
 extends Control
 
 #export(NodePath) var player
 
+## The current HP of the player.
+# @setter set_hp()
+# @type int
 var hp := 0 setget set_hp
+
+## The current floor.
+# @setter set_current_floor()
+# @type int
 var current_floor := "" setget set_current_floor
+
+## How many robots are destroyed.
 var robots_destroyed := 0 setget set_robots_destroyed
 
 var HPLabel : Label
@@ -17,6 +29,9 @@ func _ready() -> void:
 	NodeMapper.map_nodes(self)
 	_after_ready = true
 
+## Connects the room with the HUD.
+# @desc When @a room exits the tree, the HUD will commence all necessary
+#       cleanup, which includes hiding itself.
 func connect_room(room: Node):
 	room.connect('tree_exiting', self, '_on_room_tree_exiting')
 
@@ -29,9 +44,6 @@ func set_hp(_hp: int) -> void:
 	hp = _hp
 	if _after_ready:
 		HPLabel.text = str(hp)
-
-#func set_max_hp(mhp: int):
-#	pass
 
 func set_robots_destroyed(rb: int) -> void:
 	robots_destroyed = rb
