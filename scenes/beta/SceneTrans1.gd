f09f4807002dd627061e9e0eff773d1b07ada6fc
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-03-30 17:22:42 -0500

    Update SceneTrans1.tscn

--- scenes/beta/SceneTrans1.gd
+++ scenes/beta/SceneTrans1.gd
@@ -3,9 +3,15 @@ extends "res://scenes/Room.gd"
 
 var gut_tester
 
-var ToNextScreen : Area2D
-var Bob : Actor
+onready var debug_overlay = $DebugOverlay
 
 func _ready() -> void:
 	if Engine.editor_hint: return
-	NodeMapper.map_nodes(self)
+	#NodeMapper.map_nodes(self)
+	debug_overlay.queue_free()
+#	debug_overlay.propagate_call('show')
+#	debug_overlay.add_stat('Player Position', $Actors/Bob, 'global_position', false)
+#	debug_overlay.add_stat('Player Camera Position', $Actors/Bob/Camera2D,
+#		'get_camera_position', true)
+#	debug_overlay.add_stat('Player Camera Center', $Actors/Bob/Camera2D,
+#		'get_camera_screen_center', true)
