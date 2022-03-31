64d3fb30d99f4852286f6bd6b7c6ba939b669169
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-03-31 05:01:33 -0500

    Removed button command components

--- scenes/components/button_commands/BC_Print.gd
+++
@@ -1,4 +0,0 @@
-extends "res://scenes/components/button_commands/ButtonCommand.gd"
-
-func do_command(flag: bool) -> void:
-	print("do_command(%s)" % flag)
--- scenes/components/button_commands/ButtonCommand.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-func do_command(_flag: bool) -> void: pass
