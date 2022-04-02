d91e7cd787f5af6f6b6c78c1db33532aaaa17c4f
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-04-02 16:04:53 -0500

    Add documentation for VarRef

--- scripts/classes/resources/VarRef.gd
+++ scripts/classes/resources/VarRef.gd
@@ -1,7 +1,18 @@
+## A basic variable reference.
 extends Reference
 class_name VarRef
 
+## The reference type.
+# @getter get_type()
+# @type int
+# @desc The value is a constant of type @class{Variant.Type}.
 var type: int = TYPE_NIL setget ,get_type
+
+## The referenced object.
+# @type Variant
+# @setter set_object(o)
+# @getter get_object()
+# @desc Can be any type.
 var object setget set_object,get_object
 
 func get_object(): return object
