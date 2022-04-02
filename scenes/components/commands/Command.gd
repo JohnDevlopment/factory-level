c08958229963764e575d3ebe541f3f963890ec95
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2022-04-02 16:03:23 -0500

    Improvements to Command
    Method 'emit_command_signal' has a default value for its parameter.

--- scenes/components/commands/Command.gd
+++ scenes/components/commands/Command.gd
@@ -8,5 +8,5 @@ export(Array, Dictionary) var arguments : Array
 
 func do_command() -> void: pass
 
-func emit_command_signal(params: Array) -> void:
+func emit_command_signal(params: Array = []) -> void:
 	emit_signal('command', params)
