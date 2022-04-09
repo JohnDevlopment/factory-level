621b3eee4aced77846844140d682d169e4e2a9b5
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-04-09 01:27:06 -0500

    Updated Level1.tscn

--- scenes/levels/Level1.gd
+++ scenes/levels/Level1.gd
@@ -1,5 +1,10 @@
 tool
 extends "res://scenes/Room.gd"
 
+onready var overlay = $DebugOverlay
+
+func _ready() -> void:
+	if Engine.editor_hint: return
+
 func _on_guard_robot_defeated() -> void:
 	$Actors/FirstDoor.do_animation('up')
