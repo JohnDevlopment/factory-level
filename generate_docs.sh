f23e46c6ef03aa2be4e06ec3e2c71c8b5ded5de3
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-06-03 21:25:27 -0500

    Update generate_docs.sh

--- generate_docs.sh
+++ generate_docs.sh
@@ -16,35 +16,40 @@ convert() {
     done
 }
 
+convertFile() {
+    local infile="$1"
+    local base=$(basename "$infile")
+    local outfile="$rootdir/docs/${base%.gd}.html"
+    $classdoc "$infile" > "$outfile" || rm -f "$outfile" 2> /dev/null
+}
+
+# Convert named files
+x=0
+for arg; do
+    if [[ $arg =~ .+/.+ ]]; then
+        echo "is a path"
+    else
+        temp=$(find "$rootdir" -name $arg)
+        if [ -z "$temp" ]; then
+            echo "Did not find file matching pattern '$arg'" >&2
+            continue
+        fi
+        arg="$temp"
+    fi
+
+    convertFile "$arg"
+    let x++
+done
+echo $x
+
+if [ $x -gt 0 ]; then
+    echo 'done'
+    exit
+fi
+
 # Registered classes or singletons
 for subdir in nodes 'nodes/commands' singletons resources; do
     cd "$rootdir/scripts/classes/$subdir" || continue
     printf "\tlooking inside $subdir...\n"
-    #$classdoc Actor.gd > /dev/null
-
-    #for infile in *.gd; do
-    #    outfile="$rootdir/docs/${outsubdirs[$subdir]}/${infile%.gd}.html"
-    #    $classdoc "$infile" > "$outfile" || rm "$outfile" 2> /dev/null
-    #done
-
     convert
 done
-
-# Autoloads under scenes/
-#cd "$rootdir/scenes/autoloads"
-#for infile in *.gd; do
-#    outfile="$rootdir/docs/singletons/${infile%.gd}.html"
-#    $classdoc "$infile" > "$outfile" || rm "$outfile" 2> /dev/null
-#done
-
-# Other documented nodes
-#cd "$rootdir"
-#exec 3<docs.txt
-
-#while read -u 3 infile; do
-#    temp=$(basename "$infile")
-#    outfile="$rootdir/docs/other/${temp%.gd}.html"; unset temp
-#    $classdoc "$infile" > "$outfile" || rm "$outfile" 2> /dev/null
-#done
-
-#exec 3<&-
