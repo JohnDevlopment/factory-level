b5766794a1845c07396348a1597db511874bb48d
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-06-23 13:31:18 -0500

    Improvements to the screenshot component
    * File dialog to choose path to output file
    
    * Color keys the default background/clear color and crops the image to remove unused space

--- scenes/components/TakeScreenshot.gd
+++ scenes/components/TakeScreenshot.gd
@@ -1,52 +1,64 @@
 extends Node
 
-signal screenshot_taken
+signal screenshot_taken()
 
-export(String, FILE) var output_file := ''
 export(float, 0.0, 1000.0, 0.1) var delay := 0.0
 
-var timer : Timer
-
-func _enter_tree() -> void:
-	if delay > 0.0:
-		timer = Timer.new()
-		timer.one_shot = true
-		timer.autostart = false
-		timer.wait_time = delay
-		add_child(timer)
+var output_file := ""
 
 func _ready() -> void:
-	var file_dir : String
-	var dir := Directory.new()
-	
-	# Expecting a file, not a directory
-	file_dir = output_file
-	if dir.dir_exists(file_dir):
-		push_error("'%s' is a directory!" % file_dir)
-		return
-	
-	# Directory not found
-	file_dir = output_file.get_base_dir()
-	if not dir.dir_exists(file_dir):
-		push_error("The directory '%s' does not exist." % file_dir)
+	if Game.has_player():
+		Game.get_player().queue_free()
 	
 	# Start timer
-	if is_instance_valid(timer):
-		timer.start()
+	if delay > 0.0:
+		var timer = get_tree().create_timer(delay)
 		yield(timer, 'timeout')
 	
-	call_deferred('_take_snapshot')
+	$CanvasLayer/FileDialog.popup_centered()
+	Game.set_paused(true)
 
 func _take_snapshot():
 	var img : Image = get_viewport().get_texture().get_data()
-	
 	img.flip_y()
+	img = _colorkey(Color("#4C4C4C"), img)
 	
 	var err := OK
-	
 	err = img.save_png(output_file)
 	if err:
 		push_error("Failed to write texture to '%s'" % output_file)
 		return
 	
+	Game.set_paused(false)
+	
 	emit_signal('screenshot_taken')
+
+func _on_FileDialog_file_selected(path: String) -> void:
+	output_file = path
+	yield(get_tree().create_timer(1), 'timeout')
+	call_deferred('_take_snapshot')
+
+func _on_FileDialog_popup_hide() -> void:
+	Game.set_paused(false)
+
+func _colorkey(key: Color, img: Image) -> Image:
+	# Add alpha channel
+	if img.detect_alpha() == img.ALPHA_NONE:
+		img.convert(Image.FORMAT_RGBA8)
+	
+	# Set each pixel matching KEY as transparent
+	var size := img.get_size()
+	img.lock()
+	for y in size.y:
+		for x in size.x:
+			if img.get_pixel(x, y) == key:
+				img.set_pixel(x, y, Color(0, 0, 0, 0))
+	img.unlock()
+	
+	# Crop the image 
+	var used_rect := img.get_used_rect()
+	if used_rect != Rect2(Vector2(), size):
+		var temp := img
+		return temp.get_rect(used_rect)
+	
+	return img
