c6e19b3469233e3ae8816a8eabb40fc1eb5494c5
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-07-29 03:29:52 -0500

    Fix hitbox/hurtbox not disabling correctly

--- scenes/components/HitboxHurtbox.gd
+++ scenes/components/HitboxHurtbox.gd
@@ -3,5 +3,6 @@ extends Area2D
 export var disabled := false setget set_disabled
 
 func set_disabled(flag: bool) -> void:
-	disabled = flag
-	$CollisionShape2D.set_deferred('disabled', flag)
+	for n in get_children():
+		if n is CollisionShape2D:
+			n.disabled = flag
